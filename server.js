bb3fcaad0564919659c359974c2ee7b47a44331d
Murod <parmonov98@yandex.ru>
2021-10-15 11:27:05 +0500

    prepare for heroku deploy

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 connectDB();
 
 app.use(express.json({ extented: true }));
 
-const PORT = process.env.PORT || 5050
+
 
 app.use("/api/users", require('./routes/users'));
 app.use("/api/auth", require('./routes/auth'));
@@ -20,4 +20,11 @@ app.get("/json", (req, res) => {
   res.json({ page: "home", content: "Content of the page" });
 });
 
+if (process.env.NODE_ENV == 'production') {
+  // 
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
+const PORT = process.env.PORT || 5050
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
